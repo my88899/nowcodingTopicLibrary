9fa8f61c761f4847fc5b1ef7f2399ed0730e2ebb
my88899 <haojiutao@gmail.com>
2017-08-23 00:46:00 +0800

    35 C SwordOffer String pairs

--- swordOffer/31整数中1出现的次数（从1到n整数中1出现的次数）/src/main/java/Solution.java
+++ swordOffer/31整数中1出现的次数（从1到n整数中1出现的次数）/src/main/java/Solution.java
@@ -1,6 +1,24 @@
 public class Solution {
     public int NumberOf1Between1AndN_Solution(int n) {
-        String str = Integer.toString(n);
-        return 6;
+        if (n == 0) {
+            return 0;
+        }
+        int high, low, cur, tmp, i=1, total = 0;
+        high = 1;
+        while (high != 0) {
+            high = n / (int)Math.pow(10, i);
+            tmp = n % (int)Math.pow(10, i);
+            cur = tmp / (int)Math.pow(10, i - 1);
+            low = tmp % (int)Math.pow(10, i - 1);
+            if (cur == 1){
+                total += high * (int)Math.pow(10, i - 1) + low + 1;
+            } else if (cur < 1) {
+                total += high * (int)Math.pow(10, i - 1);
+            } else {
+                total += (high + 1) * (int)Math.pow(10, i - 1);
+            }
+            i++;
+        }
+        return total;
     }
 }
